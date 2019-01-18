490b4118109676d74413521114297a68e4107c5a
iqDF <daniel.thekurniadi@gmail.com>
2019-01-18 23:37:17 +0800

    Cpp 1 - print and scan input outputs

---
+++ galperin-bounce/galperin.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+
+int main()
+{
+    float v_m = 0.0;
+    float x, v_M;
+
+    std::cout << "Welcome to Galperin Bounce Simulation" << std::endl;
+
+    std::cout << "Enter initial velocity for big box: ";
+    std::cin >> v_M;
+
+    std::cout << "Enter the mass for big box: ";
+    std::cin>> x;
+
+    std::cout << "Calculating..." << std::endl;
+    
+}
